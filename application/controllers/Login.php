95f22b0da5753ca52af023ab333227d0237326ba
Jeri Ramadhan <jeriramadhan25@gmail.com>
2021-04-05 15:49:49 +0700

    login

---
+++ application/controllers/Login.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Login extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model('core/MY_Model');
+    }
+    public function index()
+    {
+        $this->load->view('login');
+    }
+    
+    function auth()
+    {
+        $username   = $this->input->post('username');
+        $password   = password_hash($this->input->post('password'), PASSWORD_BCRYPT, array('cost' => 10));
+        $check      = $this->MY_Model->singleData('user', array('username' => $username));
+        
+        
+        if ($check == TRUE) {
+            if (password_verify($password, $check->password)) {
+                session_start();
+                $_SESSION['username'] = $username;
+                redirect('admin/user/index');
+                die();
+            } else {
+                echo '<script language="javascript">
+                        window.alert("LOGIN GAGAL! Silakan coba lagi");
+                        window.location.href="./";
+                    </script>';
+            }
+        } else {
+            echo '<script language="javascript">
+                        window.alert("Username tidak tersedia");
+                        window.location.href="./";
+                    </script>';
+        }
+    }
+}
