5a9229e4f1a950893d856ff7b391e464a324ebe3
Syafiq Roikhan <msyafiq.roikhanm@gmail.com>
2021-04-27 13:56:45 +0700

    forntend home

--- application/views/home.php
+++ application/views/home.php
@@ -2,56 +2,34 @@
             <!-- ===== Page-Container ===== -->
             <div class="container-fluid">
               <div class="row">
-                <div class="col-md-8 col-sm-12">
-                  <div class="row">
-                    <div class="col-sm-4">
-                      <div class="white-box small-box-widget">
-                        <ul class="list-inline row">
-                          <li class="col-xs-3 p-t-10">
-                            <div class="icon-box bg-primary">
-                              <i class="icon-bag"></i>
-                            </div>
-                          </li>
-                          <li class="col-xs-9 p-l-20">
-                            <h4>Pengunjung Hari Ini</h4>
-                            <div class="ct-sales-chart"></div>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="col-sm-4">
-                      <div class="white-box small-box-widget">
-                        <ul class="list-inline row">
-                          <li class="col-xs-3 p-t-10">
-                            <div class="icon-box bg-success">
-                              <i class="icon-user"></i>
-                            </div>
-                          </li>
-                          <li class="col-xs-9 p-l-20">
-                            <h4>Jumlah Artikel</h4>
-                            <div class="ct-uq-chart chart-pos"></div>
-                          </li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="col-sm-4">
-                      <div class="white-box small-box-widget">
-                        <div class="p-t-10 p-b-10">
-                          <div class="icon-box bg-warning">
-                            <i class="icon-refresh"></i>
-                          </div>
-                          <div class="detail-box">
-                            <h4>Jumlah Komentar<span class="pull-right text-warning font-22 font-normal">42%</span></h4>
-                            <div class="progress">
-                              <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="42"
-                                aria-valuemin="0" aria-valuemax="100" style="width: 42%">
-                                <span class="sr-only">42% Complete</span>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                <div class="col-lg-4 col-sm-3 col-xs-12">
+                  <div class="white-box analytics-info">
+                    <h3 class="box-title">Pengunjung Hari Ini</h3>
+                    <ul class="list-inline">
+                      <li class="text-center">
+                        <span class="counter text-default" style="font-size: 30px;font-weight:bold;">8659</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-sm-3 col-xs-12">
+                  <div class="white-box analytics-info">
+                    <h3 class="box-title">Jumlah Artikel</h3>
+                    <ul class="list-inline">
+                      <li class="text-center">
+                        <span class="counter text-default" style="font-size: 30px;font-weight:bold;">8659</span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-sm-3 col-xs-12">
+                  <div class="white-box analytics-info">
+                    <h3 class="box-title">Jumlah Komentar</h3>
+                    <ul class="list-inline">
+                      <li class="text-center">
+                        <span class="counter text-default" style="font-size: 30px;font-weight:bold;">8659</span>
+                      </li>
+                    </ul>
                   </div>
                 </div>
               </div>
@@ -71,10 +49,10 @@
                         </select>
                         <ul class="list-inline">
                           <li>
-                            <h6 class="font-15"><i class="fa fa-circle m-r-5 text-success"></i>New Sales</h6>
+                            <h6 class="font-15"><i class="fa fa-circle m-r-5 text-success"></i>Pengunjung</h6>
                           </li>
                           <li>
-                            <h6 class="font-15"><i class="fa fa-circle m-r-5 text-primary"></i>Existing Sales</h6>
+                            <h6 class="font-15"><i class="fa fa-circle m-r-5 text-primary"></i>Komentar</h6>
                           </li>
                         </ul>
                       </div>
