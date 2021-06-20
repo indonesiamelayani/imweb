e4027ce8d90e5a0a8014b8bc3fe67ae591bb876d
Jeri Ramadhan <jeriramadhan25@gmail.com>
2021-06-20 22:52:13 +0700

    rss tempo

--- application/views/public/article.php
+++ application/views/public/article.php
@@ -1,3 +1,7 @@
+<?php
+include('FeedParser.php');
+$Parser = new FeedParser();
+?>
 <div class="container">
   <div class="row">
     <div class="col-12 col-md-5 ml-auto">
@@ -176,15 +180,53 @@
         <div class="col-md-12 col-sm-12">
           <div class="row">
             <div class="col-12 my-2">
-              <iframe src="//rss.bloople.net/?url=https%3A%2F%2Fwww.antaranews.com%2Frss%2Fterkini.xml&detail=30&limit=1&showtitle=false&showicon=true&showempty=true&type=html"></iframe>
-              <!-- <img class="w-100" src="assets/img/img-info1.png" alt=""> -->
+              <?php
+              $Parser->parse('https://rss.tempo.co/nasional');
+              $channels = $Parser->getChannels();
+              $items = $Parser->getItems();
+
+              ?>
+              <div class="col-md-12 col-sm-12">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                  <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                  </ol>
+                  <h5>Berita Terkini</h5>
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="assets/img/logo-im.jpg" height="178px">
+                    </div>
+                    <?php foreach (array_slice($items, 0, 5) as $item) : ?>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="<?php echo $item['IMG']; ?>" height="178px">
+                        <div class="carousel-caption d-none d-md-block">
+                          <h5><?php echo $item['TITLE']; ?></h5>
+                          <a href="<?php echo $item['LINK']; ?>" class="btn btn-sm btn-danger">Baca</a>
+                        </div>
+                      </div>
+                    <?php endforeach; ?>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+              </div>
             </div>
-            <div class="col-12">
-              <img class="w-100" src="assets/img/img-info2.png" alt="">
-            </div>
+
+          </div>
+          <div class="col-12">
+            <img class="w-100" src="assets/img/img-info2.png" alt="">
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
 </div>
